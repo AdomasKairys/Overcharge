375cb08e62225a3529a9948f49dff314343da091
Adomas Kairys <adomas.kairys@ktu.edu>
2024-03-01 12:44:10 +0200

    added rotation to ball

--- Assets/Scripts/BallRotation.cs
+++ Assets/Scripts/BallRotation.cs
@@ -12,9 +12,9 @@ public class BallRotation : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.rotation = Quaternion.LookRotation(rb.velocity, transform.up);
-        transform.Rotate(rb.velocity.normalized, Space.World);
+        //transform.rotation = Quaternion.LookRotation(rb.velocity, transform.up);
+        transform.Rotate(new Vector3 (rb.velocity.z, rb.velocity.y, -rb.velocity.x), Space.World);
     }
 }
