bf607803a57907246e3cce6f0ea1cace03ebdb84
Lukas Vasiliauskas <lukas.vasi@gmail.com>
2024-03-03 19:52:18 +0200

    Fixed retagging by adding a tag cooldown mechanism

--- Assets/Scripts/TagController.cs
+++ Assets/Scripts/TagController.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class TagController : MonoBehaviour
 {
+    [Header("Tag cooldown in seconds")]
+    public float tagCooldown = 2.0f;
+    
     private PlayerStateController thisStateController;
+    private bool blocked; // whether the tagging functionality for this player is blocked
 
     // Start is called before the first frame update
     void Start()
     {
         thisStateController = GetComponentInParent<PlayerStateController>();
+        blocked = false;
         Debug.Log(transform.parent.gameObject.name + ": " + thisStateController);
     }
 
@@ -25,19 +30,40 @@ public class TagController : MonoBehaviour
         if (other.CompareTag("TagTrigger")) // reduntant check for future proofing
         {
             PlayerState thisState = thisStateController.GetState();
-            // Tag control is performed only when this player is a chaser
-            if(thisState == PlayerState.Chaser)
+            // Tag control is performed only when this player is an unblocked chaser
+            if(!blocked && thisState == PlayerState.Chaser)
             {
                 PlayerStateController otherStateController = other.gameObject.GetComponentInParent<PlayerStateController>();
+                TagController otherTagController = other.GetComponentInParent<TagController>();
                 PlayerState otherState = otherStateController.GetState();
-                // If the tagged player is a runner
-                if (otherState == PlayerState.Runner)
+                bool otherBlocked = otherTagController.IsBlocked();
+                // If the tagged player is an unblocked runner
+                if (!otherBlocked && otherState == PlayerState.Runner)
                 {
+                    Block();
+                    otherTagController.Block();
                     thisStateController.SetState(PlayerState.Runner);
                     otherStateController.SetState(PlayerState.Chaser);
                     Debug.Log(transform.parent.gameObject.name + " tagged " + other.gameObject.name + " State changed to " + thisStateController.GetState());
                 }
             }
         }
+    }
+
+    public bool IsBlocked()
+    {
+        return blocked;
+    }
+
+    public void Block()
+    {
+        blocked = true;
+        StartCoroutine(UnblockAfterCooldown());
+    }
+
+    IEnumerator UnblockAfterCooldown()
+    {
+        yield return new WaitForSeconds(tagCooldown); // Wait for the cooldown time.
+        blocked = false; // Unblock the player.
     }
 }
